82f6b4eacf8fe92ce4a3005c3d4196b5d012acbc
Jared Mejia <jaredmejia4440@gmail.com>
2023-01-29 19:15:42 -0600

    app done

--- client/src/components/messenger/EachMessage.tsx
+++ client/src/components/messenger/EachMessage.tsx
@@ -1,12 +1,27 @@
-import React from 'react';
-import { Message } from '../../types/messages.types';
+import React from "react";
+import { Message } from "../../types/messages.types";
+import { User } from "../../types/users.types";
 
-const EachMessage = ({message}: {message: Message}) => {
-    return (
-        <div>
-            {message.message}
-        </div>
-    );
+const EachMessage = ({
+  message,
+  currentUser,
+}: {
+  message: Message;
+  currentUser: User;
+}) => {
+
+  return (
+    <div
+      className={`${
+        message.userId === currentUser.data?.user.id
+          ? "self-end bg-[#005C4B]"
+          : ""
+      }
+      p-4 rounded-2xl bg-slate-600 w-fit max-w-sm`}
+    >
+      <p>{message.message}</p>
+    </div>
+  );
 };
 
 export default EachMessage;
