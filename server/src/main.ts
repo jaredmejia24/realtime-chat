3458b40a6e99d2b4eea4ec865e78e534c55ccc3e
Jared Mejia <jaredmejia4440@gmail.com>
2023-04-02 19:38:13 -0600

    static file rendering

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,6 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import * as cookieParser from 'cookie-parser';
-import { join } from 'path';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -16,27 +15,6 @@ async function bootstrap() {
       transformOptions: { enableImplicitConversion: true },
     }),
   );
-
-  const options = {
-    dotfiles: 'ignore',
-    etag: false,
-    extensions: [
-      'htm',
-      'html',
-      'css',
-      'js',
-      'ico',
-      'jpg',
-      'jpeg',
-      'png',
-      'svg',
-    ],
-    index: ['index.html'],
-    maxAge: '1m',
-    redirect: false,
-  };
-
-  app.useStaticAssets(join(__dirname, '..', 'public'), options);
 
   app.enableCors({ origin: true, credentials: true });
 
